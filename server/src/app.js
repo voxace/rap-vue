496c237e09b20137836350627fa9ecd1b6f27f0d
voxace <dave@voxace.com>
2018-06-12 21:57:52 +1000

    Base setup done

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req,res) => {
+  res.send('hello world')
+})
+
+app.listen(8081)
